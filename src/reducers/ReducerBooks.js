ba588666c114afc13b39134e5500da27cb9c2c42
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-03 10:11:47 -0700

    update the books reducer and bookdetail  to include pages

--- src/reducers/ReducerBooks.js
+++ src/reducers/ReducerBooks.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'JavaScript the good parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'}
+    { title: 'JavaScript the good parts', pages: 100 },
+    { title: 'Harry Potter', pages: 50 },
+    { title: 'The Dark Tower', pages: 35 },
+    { title: 'Eloquent Ruby', pages: 1 }
   ];
 }
