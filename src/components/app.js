6c100d618094395a11265912038eac3544cd011e
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-02 23:03:33 -0700

    create Booklist container, create a BookReducer, connect the two using react-redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import BookList from '../containers/BookList';
+import { connect } from 'react-redux';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return ({
+    books: state.books
+  });
+}
+
+export default connect(mapStateToProps)(BookList);
